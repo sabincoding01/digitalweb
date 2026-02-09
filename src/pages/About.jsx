6a7bdc388d1404801e34821c3d8a7d244ede6880
sabincoding01 <sabintimalsina74@gmail.com>
2026-02-10 00:04:39 +0545

    navbar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
-export default About
+export default About;
